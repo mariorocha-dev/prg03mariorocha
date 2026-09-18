93f2f4f66c5172d7ad3206ff131308a4f7c17d22
mariorocha-dev <20231ireads0015@ifba.edu.br>
2026-09-18 17:14:02 -0300

    Adicionando encapsulamento

---
+++ prg03mariorocha/src/main/java/br/com/ifba/usuario/interfaces/Autenticavel.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.usuario.interfaces;
+
+/**
+ *
+ * @author mariorocha
+ */
+public interface Autenticavel {
+    boolean autenticar(String login, String senha);
+}
